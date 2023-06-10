a82611778d27fddc447a1ea94a7b25c92f4299c0
Gabriel Sferra <gfsferra35@gmail.com>
2023-06-10 18:06:12 -0300

    Project beggining

---
+++ Migrations/20230510130435_InitialCreate.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BondGarfoApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categoria",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", nullable: true),
+                    Descricao = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categoria", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Prato",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", nullable: true),
+                    Ingredientes = table.Column<string>(type: "TEXT", nullable: true),
+                    categoria = table.Column<string>(type: "TEXT", nullable: true),
+                    Preco = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Validade = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Prato", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Categoria");
+
+            migrationBuilder.DropTable(
+                name: "Prato");
+        }
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,25 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+window.addEventListener("DOMContentLoaded", addCanonical(window.location.href))
+
+function addCanonical(url) {
+  let link = document.createElement("link")
+  link.setAttribute("rel", "canonical")
+  link.href = url
+  document.head.appendChild(link)
+}
+
+function accordion(button) {
+  button.classList.toggle("active")
+  var panel = button.nextElementSibling;
+  if (panel.style.maxHeight) {
+    panel.style.maxHeight = null;
+    panel.style.marginTop = null;
+  } else {
+    panel.style.maxHeight = panel.scrollHeight + "px";
+    panel.style.marginTop = "12px";
+  }
+}
